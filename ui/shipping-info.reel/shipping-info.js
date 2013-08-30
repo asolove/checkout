0bb31e2fced67a31f2f3e08dc44a582afd348ae1
Adam Solove <asolove@gmail.com>
2013-08-30 10:35:51 -0400

    Bindings to show total order price.

---
+++ ui/shipping-info.reel/shipping-info.js
@@ -0,0 +1,17 @@
+/**
+ * @module ./shipping-info.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class ShippingInfo
+ * @extends Component
+ */
+exports.ShippingInfo = Component.specialize(/** @lends ShippingInfo# */ {
+    constructor: {
+        value: function ShippingInfo() {
+            this.super();
+        }
+    }
+});
