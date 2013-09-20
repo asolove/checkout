3ed94fe49fe6948cd16831db8c85771d9713adef
Adam Solove <asolove@gmail.com>
2013-09-20 11:52:12 -0400

    Some manual changes to make things look nicer.

--- ui/product.reel/product.js
+++ ui/product.reel/product.js
@@ -2,7 +2,8 @@
  * @module ./product.reel
  * @requires montage/ui/component
  */
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    PressComposer = require("montage/composer/press-composer").PressComposer;
 
 /**
  * @class Product
@@ -12,6 +13,35 @@ exports.Product = Component.specialize(/** @lends Product# */ {
     constructor: {
         value: function Product() {
             this.super();
+            this._pressComposer = new PressComposer();
+            this._pressComposer.delegate = this;
+            this.addComposer(this._pressComposer);
+        }
+    },
+
+    prepareForActivationEvents: {
+        value: function() {
+            this._pressComposer.addEventListener("press", this, false);
+        }
+    },
+
+    _pressComposer: {
+        value: null
+    },
+
+    expanded: {
+        value: false
+    },
+
+    handlePress: {
+        value: function() {
+            if(!this.expanded) this.expanded = true;
+        }
+    },
+
+    surrenderPointer: {
+        value: function(pointer, component) {
+            return false;
         }
     }
 });
